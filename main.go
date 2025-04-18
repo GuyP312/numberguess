fd8220cfc2952080af49b29ed35de24f52f55b6d
GuyP <guyparnchinda@gmail.com>
2025-04-19 01:12:47 +0700

    add comments

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 )
 
 func randInt(cap int) int {
-	seed := time.Now().UnixNano()
-	source := rand.NewSource(seed)
-	randInstance := rand.New(source)
-	goal := randInstance.Intn(cap + 1)
+	seed := time.Now().UnixNano()      // get current timestamp in nanosecond
+	source := rand.NewSource(seed)     // create new source of randomess from seed value which is timestamp
+	randInstance := rand.New(source)   // create new random number generator
+	goal := randInstance.Intn(cap + 1) // generate random number between 0 and cap + 1
 	return goal
 }
 
@@ -23,7 +23,7 @@ func main() {
 	goal := randInt(cap)
 	guess := 0
 
-	maximumAttempts := int(math.Ceil(math.Log2(float64(cap))))
+	maximumAttempts := int(math.Ceil(math.Log2(float64(cap)))) // log2 the cap and round up to the nearest integer
 	for try := 0; try < maximumAttempts; try++ {
 		fmt.Print("Please Enter your guess:")
 		fmt.Scan(&guess)
